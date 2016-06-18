5790dd0ea9b51da35fee66f24b7823cfec1cacc6
Nik K <root@965251559.acs.edcc.edu>
2016-06-17 21:03:37 -0700

    Made some progress on the final...

--- final.sh
+++ final.sh
@@ -1,9 +1,74 @@
 #!/bin/bash
 
 . ~/bin/library.sh
+trap "TRAP_C" SIGINT
+trap "TRAP_T" SIGTERM
 
-if_exiv2
-echo "exif is installed!"
-if_exiftool
-echo "exiftool is installed!"
+# Processing arguments:
+while getopts :fhd OPT; do
+  case "$OPT" in
+    d) set -x;;
+    f) FORCE="YES";;
+    h) HELP="WANTED";;
+    *) echo "Unknown argument detected. Please run ${0##*/} -h for correct usage information."; exit 1;;
+  esac
+done
 
+# Removing options, keeping additional arguments:
+shift $[ $OPTIND - 1 ]
+
+# Processing -h option:
+if [ ! -z $HELP ]; then
+  echo -e "This script is designed to create a Year/Month/Day structure inside of a DIRECTORY based on \nthe creation date of the media files in the DIRECTORY, and copy/move files accordingly."
+  echo "Usage:"
+  echo "	${0##*/} DIRECTORY        Creates the Year/Month/Day structure and copies the mediafiles"
+  echo "	${0##*/} -f DIRECTORY     Creates the Year/Month/Day structure and moves the mediafiles"
+  echo "	${0##*/} -h               Prints this message and exits"
+  echo "        ${0##*/} -d               Turns on debugging information"
+  echo
+  exit
+fi
+
+# Checking if input parameter has been supplied and providing help messages if needed:
+if [ -z "${1}" ]
+  then
+    echo "Please provide a directory for script to work on. For usage information run ${0##*/} -h."
+    exit 123
+  elif [[ (! -d $1) || (! -r $1) ]]
+    then
+      echo "Unable to open ${1} for reading, exiting..."
+      exit 403
+fi
+
+# Checking if packages are installed:
+echo -e "Checking if all necessary packages are installed. Please wait as this may take up to a minute."
+if_exiv2 INSTALL
+if_exiftool INSTALL
+
+# Down below is the meat of the script, but it's not there yet... Stay tuned for more! :D
+
+
+
+
+
+
+
+# Testing the remove procedures:
+read -p "Do you want to remove the exiv2 now? (y/N)" -n 1 ANS1
+if [[ ${ANS1} = [Y/y] ]]
+  then
+    echo
+    if_exiv2 REMOVE
+  else
+    echo "Skipping..."
+fi
+
+read -p "Do you want to remove the exiftool now? (y/N)" -n 1 ANS2
+if [[ ${ANS2} = [Y/y] ]]
+  then
+    echo
+    if_exiftool REMOVE
+  else
+    echo "Nothing to do!"
+fi
+echo "Done!"
--- library.sh
+++ library.sh
@@ -1,41 +1,111 @@
 #!/bin/bash
 # This function library goes with final.sh script.
 
-# Checking if exiv2 is installed and installing if necessary:
-if_exiv2()
+TRAP_C()
 {
-exiv2 &>/dev/null
-if [[ ${?} = 127 ]]
-  then
-    logger "Installing exiv2 in preparation for running ${0} script."
-    yum -y install exiv2 &>~/exiv2.log
-  else :
-fi
+echo "   Please wait for the script to finish!"
 }
 
+TRAP_T()
+{
+echo "   OK, reverting the changes, please wait..."
+if_exiv2 REMOVE
+if_exiftool REMOVE
 
-# Checking if exiftool is installed and installing if necessary:
+
+
+}
+
+# Function to install/remove exiv2:
+if_exiv2()
+{
+if [ ${1} == INSTALL ]
+  then
+    exiv2 &>/dev/null
+    if [[ ${?} = 127 ]]
+      then
+        logger "Installing exiv2 in preparation for running ${0} script."
+        echo "Exiv2 was not found on your system; installing..." | tee ~/exiv2.log
+        yum -y install exiv2 &>>~/exiv2.log
+        if [[ ${?} = 0 ]]
+          then
+            echo "The package exiv2 was installed successfully!" | tee -a ~/exiv2.log
+        fi
+      else
+        echo "Exiv2 appears to be installed and is working properly, continuing..." | tee -a ~/exiv2.log
+    fi
+fi
+# Removing the exiv2:
+if [ ${1} == REMOVE ]
+  then
+    logger "Removing the exiv2 using the ${0} script."
+    echo "Removing the exiv2 package..." | tee -a ~/exiv2.log
+    yum -y remove exiv2 &>>~/exiv2.log
+    if [[ ${?} = 0 ]]
+      then
+        echo "The package exiv2 was removed successfully!" | tee -a ~/exiv2.log
+    fi
+fi
+} 2>>~/exiv2.log
+
+
+# Function to install/remove exiftool:
 if_exiftool()
 {
-exiftool ${0} &>/dev/null
-if [[ ${?} = 127 ]]
+if [ ${1} == INSTALL ]
   then
-    logger "Installing exiftool in preparation for running ${0} script."
-    yum -y install perl-ExtUtils-MakeMaker &>~/perl-ExtUtils.log
-    # Need to confirm the above successed.
-    TMP1=$(mktemp -d)
-    cd ${TMP1}
-    wget http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-10.20.tar.gz
-    gzip -dc Image-ExifTool-10.20.tar.gz | tar -xf -
-    cd Image-ExifTool-10.20/
-    perl Makefile.PL
-    make test
-    # Need to check if testing is successfull and quit if necessary.
-    make install
-    # Need to verify installation exit code
-    cd
-    rm -rf ${TMP1}
-  else :
+    exiftool ${0} &>/dev/null
+    if [[ ${?} = 127 ]]
+      then
+        logger "Installing exiftool in preparation for running ${0} script."
+        echo "ExifTool was not found on your system; installing..." | tee ~/exiftool.log
+        # Installing required package perl-ExtUtils-MakeMaker:
+        if [[ ! $(rpm -q perl-ExtUtils-MakeMaker) = 0 ]]
+          then
+            echo "Installing required package perl-ExtUtils-MakeMaker..." | tee ~/perl-ExtUtils.log
+            yum -y install perl-ExtUtils-MakeMaker &>>~/perl-ExtUtils.log
+        fi
+        # Installing exiftool from source: http://www.sno.phy.queensu.ca/~phil/exiftool/
+        # The following installs a specific version of Image-ExifTool.
+        # If the command fails then verify if program version haven't changed.
+        ExifVer=Image-ExifTool-10.20
+        TMP1=$(mktemp -d)
+        cd ${TMP1}
+        echo "Downloading ExivTool installation files..." | tee -a ~/exiftool.log
+        wget http://www.sno.phy.queensu.ca/~phil/exiftool/${ExifVer}.tar.gz &>>~/exiftool.log
+        echo "Extracting ExivTool installation files..." | tee -a ~/exiftool.log
+        gzip -dc ${ExifVer}.tar.gz | tar -xf - &>>~/exiftool.log
+        cd ${ExifVer}
+        perl Makefile.PL &>>~/exiftool.log
+        echo "Performing internal tests before installation (shouldn't take more than 30 sec)..." | tee -a ~/exiftool.log
+        make test &>>~/exiftool.log
+        if [ ! $? == 0 ]
+          then
+            echo "The tests were not successful. Please review exiftool.log for errors."
+          else
+            echo "The tests comleted successfully!"
+        fi
+        echo "Continuing with ExifTool installation..."
+        make install &>>~/exiftool.log
+        if [[ ${?} = 0 ]]
+          then
+            echo "ExifTool was installed successfully!"
+        fi
+      else
+        echo "ExifTool appears to be installed and is working properly, continuing..." | tee -a ~/exiftool.log
+    fi
 fi
-} &>~/exiftool.log
-
+# Removing exiftool:
+if [ ${1} == REMOVE ]
+  then
+    logger "Removing the exiftool using the ${0} script."
+    echo "Removing ExifTool..." | tee -a ~/exiftool.log
+    UNINSTALL=$(mktemp)
+    cd ${TMP1}/${ExifVer}/
+    make uninstall | grep -i unlink > ${UNINSTALL}
+    . ${UNINSTALL}
+    cd ~
+    rm -rf ${TMP1}
+    rm -f ${UNINSTALL}
+fi
+} 2>>~/exiftool.log
